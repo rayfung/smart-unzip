202f967643febbb75ea372ad44a3a1af35bb6ce8
Ray Fung <posixfung@163.com>
2013-05-30 00:21:42 +0800

    rename scripts

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ cd "$tempdir"
 
 SCRIPT_LOC=$(pwd)
 
-cp -v -f "$SCRIPT_LOC"/nautilus-scripts/{*,.??*} ~/.gnome2/nautilus-scripts
+cp -v -f "$SCRIPT_LOC"/nautilus-scripts/* ~/.gnome2/nautilus-scripts
+mv ~/.gnome2/nautilus-scripts/unzip-tool.py ~/.gnome2/nautilus-scripts/.unzip-tool.py
